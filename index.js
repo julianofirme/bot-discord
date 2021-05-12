ce918b1f7822687b03cf06df9cdccff000a86196
jfirme-sys <julianofirme23@gmail.com>
2021-05-12 12:10:39 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const ytdl = require('ytdl-core') 
+
+const prefix = "!"
+
+const token = 'ODQyMDAyNjExNTMxNDE1NTky.YJu-Cg.dbZGEF0nfeKD9YfdDHmOtj41Upk'
+
+const servers = {
+    'server': {
+        connection: null,
+        dispatcher: null
+    }
+}
+
+
+client.on("message", async (msg) => {
+
+    //Ignora msg vindas de fora do servidor
+    if (!msg.guild) return
+
+    //Ignora a msg caso ela não inicie com o prefixo 
+    if(!msg.content.startsWith(prefix)) return
+
+    if(!msg.member.voice.channel) return
+
+    //Entra no canal e da play na msc
+    if(msg.content.startsWith(prefix + "play")){
+      console.log("entrando")
+      servers.server.connection = await msg.member.voice.channel.join()
+
+      //Tira o play da var link
+      let link = msg.content.slice(6)
+
+      //Toca link
+      servers.server.dispatcher = servers.server.connection.play(ytdl(link))
+      var yt= ytdl(link)
+      
+      var nomeMusica = ytdl.getVideoID(link)
+      console.log(nomemusica)
+      
+      
+      //Bucetinha só apos entrar em link
+      if(link) {
+        msg.channel.send("Tocando: "+nomeMusica)
+      }
+    }
+
+    //Disconecta do servidor de quem mandou a mensagem
+    if(msg.content === prefix + "ds"){
+      servers.server.connection = msg.member.voice.channel.leave()
+      console.log("saindo")
+    }
+
+    //Pausa a msc
+    if(msg.content === prefix + "pause") {
+      await servers.server.dispatcher.pause()
+
+      //Retorna a msc
+      if(msg.content === prefix + "resume") {
+        servers.server.dispatcher.resume()
+      } 
+    }
+
+    //Para a música
+    if(msg.content === prefix + "stop") {
+      servers.server.dispatcher.pause()
+    } 
+})  
+
+client.login(token)
